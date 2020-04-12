02ad3ea70c47e7fe6fb4c7cc315242ad56226f68
luxemk <kevin.luxem@dzne.de>
2020-04-12 12:13:40 +0200

    util folder

---
+++ vame/util/auxiliary.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  2 12:28:35 2019
+
+@author: luxemk
+"""
+import os, pickle, yaml
+import pandas as pd
+from pathlib import Path
+import numpy as np
+import networkx as nx
+
+import ruamel.yaml
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import cv2
+
+
+def create_config_template():
+    """
+    Creates a template for config.yaml file. This specific order is preserved while saving as yaml file.
+    """
+    import ruamel.yaml
+    yaml_str = """\
+# Project name
+    Project:
+    \n
+# Project path and videos
+    project_path:
+    video_sets:
+    \n
+# Spatial model
+    resnet:
+    resnet_pretrained:
+    batch_size_spatial:  
+    Epochs_spatial:
+    ZDIMS_spatial:
+    BETA_spatial: 
+    Learning_rate_spatial:
+    anneal_function: 
+    \n
+# Temporal model:
+    num_features:
+    batch_size_temporal:
+    Epochs_temporal:
+    rnn_model:
+    BETA_temporal:
+    ZDIMS_temporal:
+    Learning_rate_temporal:
+    temporal_window: 
+    future_decoder:
+    future_steps:
+    \n
+# Util:
+    TestFraction:
+    model_convergence:
+    """
+    ruamelFile = ruamel.yaml.YAML()
+    cfg_file = ruamelFile.load(yaml_str)
+    return(cfg_file,ruamelFile)
+
+
+def read_config(configname):
+    """
+    Reads structured config file
+    """
+    ruamelFile = ruamel.yaml.YAML()
+    path = Path(configname)
+    if os.path.exists(path):
+        try:
+            with open(path, 'r') as f:
+                cfg = ruamelFile.load(f)
+        except Exception as err:
+            if err.args[2] == "could not determine a constructor for the tag '!!python/tuple'":
+                with open(path, 'r') as ymlfile:
+                  cfg = yaml.load(ymlfile,Loader=yaml.SafeLoader)
+                  write_config(configname,cfg)
+    else:
+        raise FileNotFoundError ("Config file is not found. Please make sure that the file exists and/or there are no unnecessary spaces in the path of the config file!")
+    return(cfg)
+    
+    
+def write_config(configname,cfg):
+    """
+    Write structured config file.
+    """
+    with open(configname, 'w') as cf:
+        ruamelFile = ruamel.yaml.YAML()
+        cfg_file,ruamelFile = create_config_template()
+        for key in cfg.keys():
+            cfg_file[key]=cfg[key]
+
+        ruamelFile.dump(cfg_file, cf)
+        
+        
+def hierarchy_pos(G, root=None, width=.5, vert_gap = 0.2, vert_loc = 0, xcenter = 0.5):
+
+    '''
+    From Joel's answer at https://stackoverflow.com/a/29597209/2966723.  
+    '''
+    if not nx.is_tree(G):
+        raise TypeError('cannot use hierarchy_pos on a graph that is not a tree')
+
+    if root is None:
+        if isinstance(G, nx.DiGraph):
+            root = next(iter(nx.topological_sort(G)))  #allows back compatibility with nx version 1.11
+        else:
+            root = random.choice(list(G.nodes))
+
+    def _hierarchy_pos(G, root, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5, pos = None, parent = None):
+        if pos is None:
+            pos = {root:(xcenter,vert_loc)}
+        else:
+            pos[root] = (xcenter, vert_loc)
+        children = list(G.neighbors(root))
+        if not isinstance(G, nx.DiGraph) and parent is not None:
+            children.remove(parent)  
+        if len(children)!=0:
+            dx = width/len(children) 
+            nextx = xcenter - width/2 - dx/2
+            for child in children:
+                nextx += dx
+                pos = _hierarchy_pos(G,child, width = dx, vert_gap = vert_gap, 
+                                    vert_loc = vert_loc-vert_gap, xcenter=nextx,
+                                    pos=pos, parent = root)
+        return pos
+
+
+    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
