c66e3f3572a08e0c6192432c254d38e2726be4e8
luxemk <kevin.luxem@dzne.de>
2020-04-13 11:10:34 +0200

    cleaning evaluate file

--- vame/model/evaluate.py
+++ vame/model/evaluate.py
@@ -47,82 +47,26 @@ def generate_reconstructions(model, test_loader, PROJECT, PROJECT_PATH):
         PROJECT_PATH+'/model/spatial_model/evaluate/'+'vae_reconstructions_{}.eps'.format(PROJECT),
         nrow=10
     )
-
-
-def generate_samples(model, test_loader, ZDIMS, PROJECT, PROJECT_PATH):
-    model.eval()
-    z_e_x = Variable(torch.randn(64, 30, 30)).cuda()
-    x_tilde = model.decoder(z_e_x)
-    
-    xx = x_tilde.cpu().detach().numpy()
-    
-    images = (x_tilde.cpu().data + 1) / 2
-
-    save_image(
-        images,
-        PROJECT_PATH+'/model/spatial_model/evaluate/'+'vae_samples_{}_rnn.eps'.format(PROJECT),
-        nrow=8
-    )
     
     
-def plot_loss(model_name, train_loss, test_loss, kl_losses, weight_values, filepath):
+def plot_loss(model_name, train_loss, test_loss, kl_losses, weight_values, filepath, log_scale=False):
     fig, (ax1, ax2) = plt.subplots(1, 2)
     fig.suptitle('Train Loss vs. Test Loss and KL-Loss vs. KL-Weight')
     ax1.set(xlabel='Epochs', ylabel='loss [%]')
     ax1.plot(train_loss, 'b', label='train_loss')
-#    ax1.set_yscale("log")
+    if log_scale:
+        ax1.set_yscale("log")
     ax1.plot(test_loss, 'r', label='test_loss')
     ax2.plot(kl_losses, 'b', label='kl_loss')
-#    ax2.set_yscale("log")
-#    ax2.plot(weight_values, 'r', label='weight_value')
+    if log_scale:
+        ax2.set_yscale("log")
     ax2.set(xlabel='Epochs', ylabel='KL-Loss')
     ax1.legend()
     ax2.legend()
     fig.savefig(filepath+'evaluate/'+'MSE-and-KL-Loss'+model_name+'.png')
     
-  
-def eval_spatial(cfg,use_gpu, model_name):
     
-    SEED = 19
-    ZDIMS = cfg['ZDIMS_spatial']
-    TEST_BATCH_SIZE = 10
-    PROJECT = cfg['Project']
-    PROJECT_PATH = cfg['project_path']
-    res = resnet(cfg['resnet'],cfg['resnet_pretrained'])
-    
-    filepath = PROJECT_PATH+'model/spatial_model/'
-    
-    if use_gpu:
-        torch.cuda.manual_seed(SEED)
-        model = VAE(ZDIMS, res).cuda()
-    else:
-        model = VAE(ZDIMS, res)
-        
-    model.load_state_dict(torch.load(cfg['project_path']+'/'+'model/spatial_model/best_model/'+model_name+'_'+cfg['Project']+'.pkl'))    
-    
-    testset = VIDEO_DATASET(cfg['project_path']+'data/train/spatial/', data='spatial-test_full.h5', toggle='test')
-    test_loader = Data.DataLoader(testset, batch_size=TEST_BATCH_SIZE, shuffle=True, drop_last=True)
-    
-    generate_reconstructions(model, test_loader, PROJECT, PROJECT_PATH)
-    generate_samples(model, test_loader, ZDIMS, PROJECT, PROJECT_PATH)
-
-    train_loss = np.load(cfg['project_path']+'/'+'model/spatial_model/losses'+'/train_losses.npy')
-    test_loss = np.load(cfg['project_path']+'/'+'model/spatial_model/losses'+'/test_losses.npy')
-#    mse_loss = np.load(cfg['project_path']+'/'+'model/spatial_model/losses'+'/mse_losses.npy')
-    kl_loss = np.load(cfg['project_path']+'/'+'model/spatial_model/losses'+'/kl_losses.npy')
-    weight_values = np.load(cfg['project_path']+'/'+'model/spatial_model/losses'+'/weight_values.npy')
-    
-    train_loss = train_loss / np.max(train_loss)
-    test_loss = test_loss / np.max(test_loss)
-    kl_losses = kl_loss #/ np.max(kl_loss)
-    weight_values = (weight_values / np.max(weight_values))*100
-
-    plot_loss(model_name, train_loss, test_loss, kl_losses, weight_values, filepath)
-    
-    return
-    
-    
-def eval_temporal(cfg, use_gpu, model_name, spatial_features):
+def eval_temporal(cfg, use_gpu, model_name):
     
     SEED = 19
     ZDIMS = cfg['ZDIMS_temporal']
@@ -146,11 +90,7 @@ def eval_temporal(cfg, use_gpu, model_name, spatial_features):
     model.load_state_dict(torch.load(cfg['project_path']+'/'+'model/temporal_model/best_model/snapshots/'+model_name+'_'+cfg['Project']+'_epoch_50.pkl'))
     model.eval() #toggle evaluation mode
     
-    if spatial_features:
-        testset = SPATIAL_FEATURES(cfg['project_path']+'data/train/temporal/', data='spatiotemporal_test_full.npy', train=False, temporal_window=TEMPORAL_WINDOW)
-    else:
-        testset = SEQUENCE_DATASET(cfg['project_path']+'data/train/temporal/', data='sequence_test_full.npy', train=False, temporal_window=TEMPORAL_WINDOW)
-   
+    testset = SEQUENCE_DATASET(cfg['project_path']+'data/train/temporal/', data='sequence_test_full.npy', train=False, temporal_window=TEMPORAL_WINDOW)
     test_loader = Data.DataLoader(testset, batch_size=TEST_BATCH_SIZE, shuffle=True, drop_last=True)
     
     train_loss = np.load(cfg['project_path']+'/'+'model/temporal_model/losses'+'/train_losses'+model_name+'.npy')
@@ -206,30 +146,17 @@ def eval_temporal(cfg, use_gpu, model_name, spatial_features):
         
     plot_loss(model_name, train_loss, test_loss, kl_losses, weight_values, filepath)
     
-    z = latent.unsqueeze(2).repeat(1, 1, 12)
-    z = z.permute(0,2,1)
-#    fig, ax = plt.subplots()
-#
-#    ax.plot(mse_loss, 'k')
-#    ax.plot(kl_loss, 'b')
-    
     return 
     
     
-def evaluate_model(config, model_name, model=None, spatial_features=False):
+def evaluate_model(config, model_name):
     """
         Evaluation of testset
     """
     config_file = Path(config).resolve()
     cfg = read_config(config_file)
     
-    if model == None:
-        model = input("Which model do you want to evaluate? spatial/temporal: ")
-        
-    if not os.path.exists(cfg['project_path']+'model/spatial_model/evaluate/') and model =='spatial':
-        os.mkdir(cfg['project_path']+'/'+'model/spatial_model/evaluate/')
-    
-    if not os.path.exists(cfg['project_path']+'model/temporal_model/evaluate/') and model =='temporal':
+    if not os.path.exists(cfg['project_path']+'model/temporal_model/evaluate/'):
         os.mkdir(cfg['project_path']+'/'+'model/temporal_model/evaluate/')
             
     use_gpu = torch.cuda.is_available()
@@ -240,20 +167,13 @@ def evaluate_model(config, model_name, model=None, spatial_features=False):
     else:
         print("CUDA is not working!")
       
-    print("\n\nEvaluation of %s model. \n" %model)
-    if model == 'spatial':
-        eval_spatial(cfg, use_gpu, model_name)
-        
-    if model == 'temporal':
-        eval_temporal(cfg, use_gpu, model_name, spatial_features)
+    print("\n\nEvaluation of %s model. \n" %model_name)   
+    eval_temporal(cfg, use_gpu, model_name)
 
-    print("You can find the results of the evaluation in the '/model/%s_model/evaluate/' folder \n"
+    print("You can find the results of the evaluation in the '/model/temporal_model/evaluate/' folder \n"
           "OPTIONS:\n" 
           "- vame.cluster_behavior() to identify behavioral motifs.\n"
-          "- re-run %s model for further fine tuning. Check again with vame.evaluate_model()" %(model,model))
-    
-    if model == 'spatial':
-        print("- Learn temporal dependencies of your latent space with vame.temporal()\n")
+          "- re-run the model for further fine tuning. Check again with vame.evaluate_model()")
     
     
     
