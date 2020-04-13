fd624093582320762f839b20eaf8cb5dd4e123e2
luxemk <kevin.luxem@dzne.de>
2020-04-13 12:55:18 +0200

    creation of model init file

---
+++ vame/model/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  3 14:53:16 2019
+
+@author: luxemk
+"""
+
+#from VAME.model.dataloader.temporal_dataloader import SEQUENCE_DATASET
+#from VAME.model.temporal.LSTM_VAE import temporal_lstm
+#from VAME.model.evaluate import evaluate_model
+from vame.model.create_training import create_trainset
