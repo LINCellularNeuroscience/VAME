41f1505022057d738e3f6986c61fb84a1d11ac1a
luxemk <kevin.luxem@dzne.de>
2020-04-13 11:33:08 +0200

    no pycache creation anymore

--- vame/initalize_project/__init__.py
+++ vame/initalize_project/__init__.py
@@ -5,5 +5,7 @@ Created on Thu Oct 31 16:55:32 2019
 
 @author: luxemk
 """
+import sys
+sys.dont_write_bytecode = True
 
-from VAME.initalize_project.new import init_new_project
+from vame.initalize_project.new import init_new_project
