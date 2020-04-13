6121452da5e33c3b673cf473c476ea5726b6b658
luxemk <kevin.luxem@dzne.de>
2020-04-13 13:08:22 +0200

    created model init file

--- vame/model/__init__.py
+++ vame/model/__init__.py
@@ -6,7 +6,10 @@ Created on Sun Nov  3 14:53:16 2019
 @author: luxemk
 """
 
+import sys
+sys.dont_write_bytecode = True
+
 #from VAME.model.dataloader.temporal_dataloader import SEQUENCE_DATASET
-#from VAME.model.temporal.LSTM_VAE import temporal_lstm
+#from VAME.model.temporal.RNN_VAE import temporal_lstm
 #from VAME.model.evaluate import evaluate_model
 from vame.model.create_training import create_trainset
