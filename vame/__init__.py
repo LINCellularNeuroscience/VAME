2a4d733ed177519c29797b3ee3818987a4cbce65
luxemk <kevin.luxem@dzne.de>
2020-04-13 11:32:18 +0200

    creation of __init__ script

---
+++ vame/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 16:32:12 2019
+
+@author: luxemk
+"""
+import sys
+sys.dont_write_bytecode = True
+      
+from vame.initalize_project import init_new_project
