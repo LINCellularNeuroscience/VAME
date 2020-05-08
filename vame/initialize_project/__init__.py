970fdf768dddfb0046d1995ca4792e92cb07e6e2
luxemk <kevin.luxem@dzne.de>
2020-05-08 10:17:59 +0200

    small name fix

---
+++ vame/initialize_project/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 16:55:32 2019
+
+@author: luxemk
+"""
+import sys
+sys.dont_write_bytecode = True
+
+from vame.initialize_project.new import init_new_project
