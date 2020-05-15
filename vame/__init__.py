cd013724a0bc91c27d962415e2fb347c86a87ca2
luxemk <kevin.luxem@dzne.de>
2020-05-15 10:57:44 +0200

    small fix

--- vame/__init__.py
+++ vame/__init__.py
@@ -8,7 +8,7 @@ Created on Thu Oct 31 16:32:12 2019
 import sys
 sys.dont_write_bytecode = True
       
-from vame.initalize_project import init_new_project
+from vame.initialize_project import init_new_project
 from vame.model import create_trainset
 from vame.model import rnn_model
 from vame.model import evaluate_model
