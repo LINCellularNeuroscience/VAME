c446a754511cddd21d7695a0e32e2855355b612c
Mackenzie Mathis <mathis@rowland.harvard.edu>
2021-03-22 21:53:01 +0100

    docstring

--- vame/util/csv_to_npy.py
+++ vame/util/csv_to_npy.py
@@ -14,30 +14,30 @@ import numpy as np
 import pandas as pd
 
 from pathlib import Path
-from vame.util.auxiliary import read_config  
+from vame.util.auxiliary import read_config
 
 def csv_to_numpy(config, datapath):
     """
-    This is a demo function to show how a conversion from the resulting pose-estimation.csv file
-    to a numpy array can be implemented.
+    This is a function to convert your pose-estimation.csv file to a numpy array.
+
     Note that this code is only useful for data which is a priori egocentric, i.e. head-fixed
     or otherwise restrained animals.
 
-    example:
-    vame.csv_to_npy('your/CSVfile/path', 'path/you/want/it/togo')
+    example use:
+    vame.csv_to_npy('pathto/your/config/yaml', 'path/toYourFolderwithCSV/')
     """
     config_file = Path(config).resolve()
     cfg = read_config(config_file)
-    
+
     path_to_file = cfg['project_path']
     filename = cfg['video_sets']
-    
+
     for file in filename:
         # Read in your .csv file, skip the first two rows and create a numpy array
         data = pd.read_csv(datapath+file+'.csv', skiprows = 2)
         data_mat = pd.DataFrame.to_numpy(data)
         data_mat = data_mat[:,1:]
-    
+
         # get the number of bodyparts, their x,y-position and the confidence from DeepLabCut
         bodyparts = int(np.size(data_mat[0,:]) / 3)
         positions = []
@@ -47,10 +47,10 @@ def csv_to_numpy(config, datapath):
             positions.append(data_mat[:,idx:idx+2])
             confidence.append(data_mat[:,idx+2])
             idx += 3
-    
+
         body_position = np.concatenate(positions, axis=1)
         con_arr = np.array(confidence)
-    
+
         # find low confidence and set them to NaN (vame.create_trainset(config) will interpolate these NaNs)
         body_position_nan = []
         idx = -1
@@ -60,9 +60,9 @@ def csv_to_numpy(config, datapath):
             seq = body_position[:,i]
             seq[con_arr[idx,:]<.99] = np.NaN
             body_position_nan.append(seq)
-    
+
         final_positions = np.array(body_position_nan)
-    
+
         # save the final_positions array with np.save()
         np.save(os.path.join(path_to_file,'data',file,file+"-PE-seq.npy"), final_positions)
         print("conversion from DeepLabCut csv to numpy complete...")
