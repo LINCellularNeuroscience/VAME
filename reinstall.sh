ca37e0c4c36100e7abfa2762d69bdc7a8cf10695
Mackenzie Mathis <mathis@rowland.harvard.edu>
2021-03-22 20:54:05 +0100

    simple install script 1.0

---
+++ reinstall.sh
@@ -0,0 +1,3 @@
+pip uninstall vame
+python3 setup.py sdist bdist_wheel
+pip install dist/vame-1.0-py3-none-any.whl
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="vame",
-    version='0.1',
+    version='1.0',
     packages=find_packages(),
     entry_points={"console_scripts": "vame = vame:main"},
     author="K. Luxem & P. Bauer",
