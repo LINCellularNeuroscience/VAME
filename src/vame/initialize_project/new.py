31889d6a44d55a98a6050fdfd44a0ec7766c2d98
vinicvaz <vinicvaz95@gmail.com>
2024-06-14 08:30:16 -0300

    pydantic model for project init

--- src/vame/initialize_project/new.py
+++ src/vame/initialize_project/new.py
@@ -24,6 +24,8 @@ import shutil
 from datetime import datetime as dt
 from vame.util import auxiliary
 from typing import List
+from vame.schemas.project import ProjectSchema
+
 
 
 def init_new_project(
@@ -124,75 +126,16 @@ def init_new_project(
         print('Copying ',src,' to ',dst)
         shutil.copy(os.fspath(src),os.fspath(dst))
 
+    new_project = ProjectSchema(
+        Project=str(project),
+        project_path=str(project_path),
+        video_sets=video_names
+    )
+    cfg_data = new_project.model_dump()
 
-    cfg_file,ruamelFile = auxiliary.create_config_template()
-    cfg_file
-
-    cfg_file['Project']=str(project)
-    cfg_file['project_path']=str(project_path)+'/'
-    cfg_file['test_fraction']=0.1
-    cfg_file['video_sets']=video_names
-    cfg_file['all_data']='yes'
-    cfg_file['load_data']='-PE-seq-clean'
-    cfg_file['anneal_function']='linear'
-    cfg_file['batch_size']=256
-    cfg_file['max_epochs']=500
-    cfg_file['transition_function']='GRU'
-    cfg_file['beta']=1
-    cfg_file['zdims']=30
-    cfg_file['learning_rate']=5e-4
-    cfg_file['time_window']=30
-    cfg_file['prediction_decoder']=1
-    cfg_file['prediction_steps']=15
-    cfg_file['model_convergence']=50
-    cfg_file['model_snapshot']=50
-    cfg_file['num_features']=12
-    cfg_file['savgol_filter']=True
-    cfg_file['savgol_length']=5
-    cfg_file['savgol_order']=2
-    cfg_file['hidden_size_layer_1']=256
-    cfg_file['hidden_size_layer_2']=256
-    cfg_file['dropout_encoder']=0
-    cfg_file['hidden_size_rec']=256
-    cfg_file['dropout_rec']=0
-    cfg_file['hidden_size_pred']=256
-    cfg_file['dropout_pred']=0
-    cfg_file['kl_start']=2
-    cfg_file['annealtime']=4
-    cfg_file['mse_reconstruction_reduction']='sum'
-    cfg_file['mse_prediction_reduction']='sum'
-    cfg_file['kmeans_loss']=cfg_file['zdims']
-    cfg_file['kmeans_lambda']=0.1
-    cfg_file['scheduler']=1
-    cfg_file['length_of_motif_video'] = 1000
-    cfg_file['noise'] = False
-    cfg_file['scheduler_step_size'] = 100
-    cfg_file['legacy'] = False
-    cfg_file['individual_parametrization'] = False
-    cfg_file['random_state_kmeans'] = 42
-    cfg_file['n_init_kmeans'] = 15
-    cfg_file['model_name']='VAME'
-    cfg_file['n_cluster'] = 15
-    cfg_file['pretrained_weights'] = False
-    cfg_file['pretrained_model']='None'
-    cfg_file['min_dist'] = 0.1
-    cfg_file['n_neighbors'] = 200
-    cfg_file['random_state'] = 42
-    cfg_file['num_points'] = 30000
-    cfg_file['scheduler_gamma'] = 0.2
-    cfg_file['softplus'] = False
-    cfg_file['pose_confidence'] = 0.99
-    cfg_file['iqr_factor'] = 4
-    cfg_file['robust'] = True
-    cfg_file['beta_norm'] = False
-    cfg_file['n_layers'] = 1
-    cfg_file['axis'] = None
-    cfg_file['parametrization'] = 'hmm'
-    cfg_file['hmm_trained'] = False
-
-    projconfigfile=os.path.join(str(project_path),'config.yaml')
+    projconfigfile=os.path.join(str(project_path), 'config.yaml')
     # Write dictionary to yaml  config file
-    auxiliary.write_config(projconfigfile,cfg_file)
+    auxiliary.write_config(projconfigfile, cfg_data)
 
     print('A VAME project has been created. \n')
     print('Now its time to prepare your data for VAME. '
