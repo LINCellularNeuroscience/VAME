ed2f1f4390e33eae96b61bd3a11f1d3db66f8f06
vinicvaz <vinicvaz95@gmail.com>
2024-06-28 09:55:45 -0300

    using append mode for logging

--- src/vame/logging/logger.py
+++ src/vame/logging/logger.py
@@ -1,6 +1,7 @@
 import logging
 from pathlib import Path
 import io
+from datetime import datetime
 
 
 class VameLogger:
@@ -36,14 +37,23 @@ class VameLogger:
 
     def add_file_handler(self, file_path: str):
         # File handler for logging to a file
-        if not Path(file_path).parent.exists():
+        #if not Path(file_path).parent.exists():
+
+        line_break = '\n'
+        if not Path(file_path).exists():
             Path(file_path).parent.mkdir(parents=True, exist_ok=True)
+            line_break = ''
 
         if self.file_handler is not None:
             self.file_handler.close()
             self.logger.removeHandler(self.file_handler)
 
-        self.file_handler = logging.FileHandler(file_path, mode='w')
+        handler_datetime = datetime.now()
+        handler_datetime = handler_datetime.strftime("%Y-%m-%d %H-%M-%S")
+        with open(file_path, "a") as f:
+            f.write(f"{line_break}[LOGGING STARTED AT: {handler_datetime}]")
+
+        self.file_handler = logging.FileHandler(file_path, mode='a')
         self.file_handler.setFormatter(logging.Formatter(self.LOG_FORMAT, self.LOG_DATE_FORMAT))
         self.logger.addHandler(self.file_handler)
 
